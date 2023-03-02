1b97c1f696e47d045a31cde4f02b6e0704886ef5
kingbino71@gmail.com <kingbino71@gmail.com>
2023-03-01 19:24:57 -0500

    tasks

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - lexicographically compare the null-terminated
+ * strings s1 and s2
+ *
+ * @s1: pointer to a first element of the string s1
+ * @s2: pointer to a first element of the string s2
+ *
+ * Return: integer greater than, equal to, or less than 0, according
+ * as the string s1 is greater than, equal to, or kess than the string s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s1[i] == s2[i])
+	{
+		i++;
+	}
+	if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
+	else
+		return (0);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @str: pointer to a first symbol of the string str
+ *
+ * Return: pointer to a resulting string str
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((i == 0 || str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == '\t'
+		    || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
+		    || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
+		    || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
+		    || str[i - 1] == '}') && (str[i]  >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
